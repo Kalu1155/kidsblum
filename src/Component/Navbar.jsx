e1a6bb5828fa7e971abad62d52ba6d5435214b1e
Kalu David <148062083+Kalu1155@users.noreply.github.com>
2025-05-19 15:52:51 +0100

    update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import babyMart from "../assets/img/mainlogo.png";
-import Link from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
