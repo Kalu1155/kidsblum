ce4f25b0c75ff42fff3bff0f8ff786860ab750e1
Kalu David <148062083+Kalu1155@users.noreply.github.com>
2025-05-30 12:49:16 +0100

    k

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from '../Component/Navbar'
 import Footer from '../Component/Footer'
 import cartImage1 from "../assets/img/product/post-card1-1.png"
